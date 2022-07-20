00ecb56737390a3aea87ae54595e2be7b1fce0a9
dementevdot <dutikyt@gmail.com>
2022-07-20 16:09:20 +0300

    a

--- My best project/Assets/Scripts/Spawner/Spawner.cs
+++ My best project/Assets/Scripts/Spawner/Spawner.cs
@@ -8,23 +8,20 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private float _secondsBetweenSpawn;
 
-    private float _elapsedTime = 0;   
-
-
-    private void Update()
+    private void Start()
     {
-        _elapsedTime += Time.deltaTime;
-
-        if (_elapsedTime >= _secondsBetweenSpawn)
-        {
-            _elapsedTime = 0;
-            Spawn();
-        }
+        StartCoroutine(Spawn());    
     }
 
-    private void Spawn()
+    private IEnumerator Spawn()
     {
-        int spawnPointNumber = Random.Range(0, _spawnPoints.Length);   
-        Instantiate(_prefab, _spawnPoints[spawnPointNumber]);   
+        WaitForSeconds timer = new WaitForSeconds(_secondsBetweenSpawn);
+
+        while (true)
+        {
+            int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+            Instantiate(_prefab, _spawnPoints[spawnPointNumber]);
+            yield return timer; 
+        }
     }
 }
